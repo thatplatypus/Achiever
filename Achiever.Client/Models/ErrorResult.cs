2cfbc025a8bb92c166abedc184083e98db6434ca
thatplatypus <tombrewer0@gmail.com>
2024-01-13 13:31:44 -0700

    Goal Details updates - Breadcrumb trail - Auto switch to Goals on successful login - Code cleanup

---
+++ Achiever.Client/Models/ErrorResult.cs
@@ -0,0 +1,12 @@
+﻿namespace Achiever.Client.Models
+{
+    public class ErrorResult<T> : ClientResult<T>
+    {
+        public ErrorResult(string message)
+        {
+            Message = message;
+            IsSuccess = false;
+            Value = default;
+        }
+    }   
+}
---
+++ Achiever.Client/Models/SuccessResult.cs
@@ -0,0 +1,12 @@
+﻿namespace Achiever.Client.Models
+{
+    public class SuccessResult<T> : ClientResult<T>
+    {
+        public SuccessResult(T value, string message = "")
+        {
+            Value = value;
+            IsSuccess = true;
+            Message = message;
+        }
+    }
+}
