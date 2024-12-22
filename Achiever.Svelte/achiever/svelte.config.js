983780e04cde4dec76b3c03a471b9210e02948a9
Tom Brewer <tombrewer@Toms-MacBook-Pro.local>
2024-12-21 19:16:09 -0700

    Use azure swa adapater

--- Achiever.Svelte/achiever/svelte.config.js
+++ Achiever.Svelte/achiever/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import azure from 'svelte-adapter-azure-swa';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,13 +6,7 @@ const config = {
 	preprocess: vitePreprocess(), // Enables preprocessing (e.g., TypeScript, PostCSS)
 
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		  }), // Default adapter, works for most environments
+		adapter: azure(), // Default adapter, works for most environments
 		alias: {
 			// Shorten import paths for common directories
 			$lib: './src/lib',
