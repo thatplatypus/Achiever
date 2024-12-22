cf7a64647b01ab24c4a6599028899de4e78173a4
Tom Brewer <tombrewer@Toms-MacBook-Pro.local>
2024-12-22 13:36:58 -0700

    Improve login ux - automatically route to goals when authenticated - route to login when not

--- Achiever.Svelte/achiever/src/routes/+page.svelte
+++ Achiever.Svelte/achiever/src/routes/+page.svelte
@@ -1,5 +1,25 @@
 <script lang="ts">
     import { goto } from "$app/navigation";
+    import { auth } from "$lib/stores/auth";
+    import { onMount } from "svelte";
 
-          goto('/goals'); 
-    </script>
+    auth.subscribe((value) => {
+        console.log(value);
+        if(value.isAuthenticated)
+            goto("/goals"); 
+    });
+
+    onMount(() => {
+       goto("/login");
+    });
+    /*
+    onMount(() => {
+    if ($auth.isAuthenticated || $auth.user?.email) {
+        goto("/goals");
+    } else {
+        goto("/login");
+    }
+    
+});
+*/
+</script>
